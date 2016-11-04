27664d4c55d0ec13068137a262975b6663049542
Andy Tyurin <mac@iMacs-iMac.local>
2016-11-04 16:00:43 +0300

    Fix push version bash script

--- bin/_pushversion.sh
+++ bin/_pushversion.sh
@@ -7,6 +7,6 @@ PREV_VERSION=$1
 git add ./../pubspec.yaml &&
 git commit -m  "Bump version" &&
 git tag "v$version" &&
-git push ${SERVER_ALIAS} ${CURRENT_BRANCH} --tags &&
+git push origin ${CURRENT_BRANCH} --tags &&
 echo "\nDone v$PREV_VERSION => v$version." &&
 exit 1
