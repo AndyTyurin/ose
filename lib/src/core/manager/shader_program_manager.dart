5577cb44be0b3a8f36ff4c5eafbe011b7c202e8a
Andy Tyurin <andy.tyurin@gmail.com>
2017-03-01 01:09:27 +0300

    Re-work shader program manager

--- lib/src/core/manager/shader_program_manager.dart
+++ lib/src/core/manager/shader_program_manager.dart
@@ -1,13 +1,55 @@
+/// SPM manages shader programs as well as keeps link to active one.
+///
+/// Be familiar with [ShaderProgram] engine's implementation variant of
+/// shader program.
+///
+/// Note, that only one shader program can be bound per draw, but usually
+/// multiply are used to show up pretty graphics.
+///
+/// There are two cases when you need to use multply shader programs for
+/// one renderable target:
+/// * Apply post-processing, which is covered by [Filter];
+/// * Particle systems (not implemented yet).
+///
+/// Each shader program must be registered before usage. It gives flexibility
+/// of controlling all programs at once to make some batch operations at
+/// preparation stage (PS).
+
 part of ose;
 
-/// Shader program manager manages shader programs.
 class ShaderProgramManager {
-  /// Active shader program.
-  ShaderProgram _activeShaderProgram;
+  final Map<String, ShaderProgram> shaderPrograms;
 
-  ShaderProgram get activeShaderProgram => _activeShaderProgram;
+  ShaderProgram _boundShaderProgram;
 
-  void set activeShaderProgram(ShaderProgram shaderProgram) {
-    _activeShaderProgram = shaderProgram;
+  ShaderProgramManager() : shaderPrograms = <String, ShaderProgram>{};
+
+  /// Bind specific shader program by [name].
+  /// Only added shader programs can be bound.
+  /// Use [add] to add shader before binding.
+  void bindShaderProgram(String name) {
+    if (shaderPrograms.containsKey(name)) {
+      _boundShaderProgram = shaderPrograms[name];
+    }
   }
+
+  /// Add shader program.
+  /// Returns [true] if has been added, [false] if not.
+  bool add(String name, ShaderProgram shaderProgram) {
+    if (shaderPrograms.containsKey(name)) return false;
+    shaderPrograms[name] = shaderProgram;
+    return true;
+  }
+
+  /// Remove shader program.
+  /// Returns [true] if has been removed, [false] if not.
+  bool remove(dynamic name) {
+    if (shaderPrograms.containsKey(name)) {
+      shaderPrograms.remove(name);
+      return true;
+    }
+    return false;
+  }
+
+  ShaderProgram get boundShaderProgram => _boundShaderProgram;
 }
